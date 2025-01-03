013ea1870a40bc7985ded068e7890d850e989288
P.Arthur <110242063+PArthur006@users.noreply.github.com>
2025-01-03 17:02:00 -0300

    Codificação de animações
    Algoritmo atualizado para troca de animações conforme o necessário.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,7 +3,8 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 const JUMP_FORCE = -300.0
-
+var is_jumping := false 
+@onready var animation := $anim as AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Adicionando gravidade
@@ -13,12 +14,21 @@ func _physics_process(delta: float) -> void:
 	# Verificando o pulo do player
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_FORCE
+		is_jumping = true
+	elif is_on_floor():
+		is_jumping = false
 
 	# Criando direção e movimento do player .
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		animation.scale.x = direction
+		if !is_jumping:
+			animation.play("run")
+		elif is_jumping:
+			animation.play("jump")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animation.play("idle")
 
 	move_and_slide()
